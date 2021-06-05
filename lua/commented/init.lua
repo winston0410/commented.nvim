5a5c6cac0d14831a86364b0f1142d8eeb5286b57
kghugo <hugosum.dev@protonmail.com>
2021-06-05 15:56:03 +0800

    Add features for uncommenting lines with different comment symbols

--- lua/commented/init.lua
+++ lua/commented/init.lua
@@ -3,7 +3,7 @@ local opts = {
     comment_padding = " ",
     keybindings = {n = "<leader>c", v = "<leader>c"},
     set_keybindings = true,
-    alt_cms = {javascript = {patterns = {'/*%s*/'}}}
+    alt_cms = {javascriptreact = {patterns = {double = '/*%s*/'}}}
 }
 
 local function commenting_lines(lines, start_line, end_line, start_symbol,
@@ -22,9 +22,9 @@ local function commenting_lines(lines, start_line, end_line, start_symbol,
     vim.api.nvim_buf_set_lines(0, start_line, end_line, false, commented_lines)
 end
 
-local function uncommenting_lines(lines, start_line, end_line, start_symbol,
-                                  end_symbol)
-    local uncommented_lines = helper.map(lines, function(line)
+local function uncommenting_lines(lines, start_line, end_line, uncomment_symbols)
+    local uncommented_lines = helper.map(lines, function(line, index)
+        local start_symbol, end_symbol = unpack(uncomment_symbols[index])
         local uncommented_line = line:gsub(start_symbol .. "%s*", "", 1)
         if end_symbol ~= "" then
             uncommented_line = uncommented_line:gsub("%s*" .. end_symbol, "")
@@ -36,69 +36,66 @@ local function uncommenting_lines(lines, start_line, end_line, start_symbol,
         .nvim_buf_set_lines(0, start_line, end_line, false, uncommented_lines)
 end
 
-local function get_comment_wrap(cs)
-    if cs:find('%%s') then
-        return cs:match('^(.*)%%s'), cs:match('^.*%%s(.*)')
-    else
-        return nil
-    end
-end
-
-local function get_lines(mode)
-    local start_line, end_line
-    local current_line = vim.api.nvim_win_get_cursor(0)[1]
-    if mode == "n" then
-        local count = vim.v.count == 0 and 0 or vim.v.count - 1
-        start_line, end_line = current_line - 1, current_line + count
-    else
-        start_line, end_line = vim.fn.line("v"), current_line
-        if start_line > end_line then
-            start_line, end_line = end_line, start_line
+local function has_matching_pattern(line, comment_patterns, uncomment_symbols)
+    local matched = false
+    for _, pattern in pairs(comment_patterns) do
+        local escaped_start_symbol, escaped_end_symbol =
+            helper.escape_symbols(helper.get_comment_wrap(pattern))
+        local escaped_pattern = escaped_start_symbol .. ".*" ..
+                                    escaped_end_symbol
+        if line:match(escaped_pattern) then
+            table.insert(uncomment_symbols,
+                         {escaped_start_symbol, escaped_end_symbol})
+            matched = true
+            break
         end
-        start_line = start_line - 1
     end
-
-    return start_line, end_line
+    return matched
 end
 
 local function toggle_comment(mode)
-    local start_line, end_line = get_lines(mode)
+    local start_line, end_line = helper.get_lines(mode)
     local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
-    local shouldComment = false
-    local filetype = vim.o.filetype
+    local should_comment = false
+    local filetype, cms = vim.o.filetype,
+                          vim.api.nvim_buf_get_option(0, 'commentstring')
 
-    local comment_start_symbol, comment_end_symbol
+    local comment_start_symbol, comment_end_symbol =
+        helper.get_comment_wrap(cms)
+    local uncomment_symbols = {}
 
-    local escaped_start_symbol, escaped_end_symbol =
-        helper.escape_symbols(comment_start_symbol),
-        helper.escape_symbols(comment_end_symbol)
+    local alt_cms_opts = opts.alt_cms[filetype] or {}
 
-    local pattern = escaped_start_symbol .. ".*" .. escaped_end_symbol
-
+    local comment_patterns = vim.tbl_extend('force', {cms = cms},
+                                            alt_cms_opts.patterns or {})
     for _, line in ipairs(lines) do
         if line ~= "" then
-            if not line:match(pattern) then
-                shouldComment = true
+            local matched = has_matching_pattern(line, comment_patterns,
+                                                 uncomment_symbols)
+            if not matched then
+                should_comment = true
                 break
             end
+        else
+            table.insert(uncomment_symbols, {"", ""})
         end
     end
 
-    if shouldComment then
-        local commentstring = opts.alt_cms[filetype].to_comment
-        if commentstring ~= "cms" then
+    -- print('check should_comment', should_comment, vim.inspect(uncomment_symbols))
+
+    if should_comment then
+        local comment_string_to_use = alt_cms_opts.to_comment or "cms"
+
+        if comment_string_to_use ~= "cms" then
             comment_start_symbol, comment_end_symbol =
-                get_comment_wrap(opts.alt_cms[filetype][commentstring])
-        else
-            comment_start_symbol, comment_end_symbol =
-                get_comment_wrap(vim.api.nvim_buf_get_option(0, 'commentstring'))
+                helper.get_comment_wrap(
+                    alt_cms_opts.patterns[comment_string_to_use])
         end
 
         commenting_lines(lines, start_line, end_line, comment_start_symbol,
                          comment_end_symbol)
     else
-        uncommenting_lines(lines, start_line, end_line, escaped_start_symbol,
-                           escaped_end_symbol)
+        uncommenting_lines(lines, start_line, end_line, uncomment_symbols)
     end
 
     if mode == 'v' then vim.api.nvim_input("<esc>") end
