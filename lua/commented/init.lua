46083f234e86b348e9f435130a5da20b6c7944f6
Peter Tri Ho <mail@petertriho.com>
2021-07-29 09:12:35 +1000

    fix: index nil value when prefer_block_comment is false

--- lua/commented/init.lua
+++ lua/commented/init.lua
@@ -214,7 +214,11 @@ local function toggle_comment(mode, line1, line2)
 				end
 			end
 
-			if opts.prefer_block_comment or (opts.lang_options[filetype] or {}).prefer_block_comment then
+			if
+				opts.prefer_block_comment
+				or opts.lang_options[filetype] and (opts.lang_options[filetype] or {}).prefer_block_comment
+			then
+
 				-- Decide what block symbol to use
 				local start_symbol, end_symbol
 				if opts.lang_options[filetype] and (opts.lang_options[filetype].cms or {}).block then
